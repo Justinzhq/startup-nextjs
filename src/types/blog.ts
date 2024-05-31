3a1d0e3d165c6ae462d4aa62b4408b3b8b853791
Justinzhq <justinzh.q@gmail.com>
2024-05-31 10:39:37 +0800

    blog

--- src/types/blog.ts
+++ src/types/blog.ts
@@ -7,9 +7,17 @@ type Author = {
 export type Blog = {
   id: number;
   title: string;
-  paragraph: string;
   image: string;
-  author: Author;
   tags: string[];
-  publishDate: string;
 };
+
+
+// export type Blog = {
+//   id: number;
+//   title: string;
+//   paragraph: string;
+//   image: string;
+//   author: Author;
+//   tags: string[];
+//   publishDate: string;
+// };
