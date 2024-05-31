61560c0460ac51691dee1e7a9081787e359f36cd
Justinzhq <justinzh.q@gmail.com>
2024-05-31 09:47:53 +0800

    Update index.tsx

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -14,10 +14,10 @@ const Hero = () => {
                 <h1 className="mb-5 text-4xl font-bold leading-tight text-black dark:text-white sm:text-5xl sm:leading-tight md:text-6xl md:leading-tight">
                   能源物联网基础设施
                 </h1>
-                <p className="mb-5 text-base font-bold leading-relaxed text-black dark:text-white sm:text-xl md:text-2xl">
+                <p className="mb-5 text-base leading-relaxed text-black dark:text-white sm:text-xl md:text-2xl">
                   更快捷的部署和扩展
                 </p>
-                <p className="mb-12 text-sm !leading-relaxed text-body-color dark:text-white sm:text-lg md:text-xl">
+                <p className="mb-12 text-xs !leading-relaxed text-body-color dark:text-white sm:text-lg md:text-xl">
                   IT/OT融合的能源监测与远程控制解决方案更加灵活、稳健且经济，致力加速能源数字化低碳转型。
                 </p>
                 {/* <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
