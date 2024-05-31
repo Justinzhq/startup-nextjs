9ec740895efbff01f76257c57c6497978386e6f3
Justinzhq <justinzh.q@gmail.com>
2024-05-31 10:18:27 +0800

    Update index.tsx

--- src/components/Blog/index.tsx
+++ src/components/Blog/index.tsx
@@ -10,12 +10,12 @@ const Blog = () => {
     >
       <div className="container">
         <SectionTitle
-          title="Our Latest Blogs"
-          paragraph="There are many variations of passages of Lorem Ipsum available but the majority have suffered alteration in some form."
+          title="viSCADA 产品套件"
+          paragraph=""
           center
         />
 
-        <div className="grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2 md:gap-x-6 lg:gap-x-8 xl:grid-cols-3">
+        <div className="grid grid-cols-1 gap-x-8 gap-y-10 md:grid-cols-2 md:gap-x-6 lg:gap-x-8 xl:grid-cols-5">
           {blogData.map((blog) => (
             <div key={blog.id} className="w-full">
               <SingleBlog blog={blog} />
