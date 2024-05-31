3482b8026f339771ca23f63e15543d22085fafd2
Justinzhq <justinzh.q@gmail.com>
2024-05-31 11:10:33 +0800

    blog

--- src/components/Blog/SingleBlog.tsx
+++ src/components/Blog/SingleBlog.tsx
@@ -9,18 +9,18 @@ const SingleBlog = ({ blog }: { blog: Blog }) => {
       <div className="group relative overflow-hidden rounded-sm bg-white shadow-one duration-300 hover:shadow-two dark:bg-dark dark:hover:shadow-gray-dark">
         <Link
           href="/blog-details"
-          className="relative block aspect-[37/22] w-full"
+          className="relative block aspect-[300/225] w-full"
         >
           {/* <span className="absolute right-6 top-6 z-20 inline-flex items-center justify-center rounded-full bg-primary px-4 py-2 text-sm font-semibold capitalize text-white">
             {tags[0]}
           </span> */}
           <Image src={image} alt="image" fill />
         </Link>
-        <div className="p-3 sm:p-8 md:px-6 md:py-8 lg:p-8 xl:px-5 xl:py-8 2xl:p-8">
+        <div className="p-3 sm:p-3 md:px-3 md:py-3 lg:p-3 xl:px-3 xl:py-3 2xl:p-3">
           <h3>
             <Link
               href="/blog-details"
-              className="mb-3 block text-base font-bold text-black hover:text-primary dark:text-white dark:hover:text-primary sm:text-base"
+              className="mb-2 block text-base font-bold text-black hover:text-primary dark:text-white dark:hover:text-primary sm:text-base"
             >
               {title}
             </Link>
