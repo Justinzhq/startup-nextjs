bb7bff1b277f7c41a1f29919e6007da9dd1819eb
Justinzhq <justinzh.q@gmail.com>
2024-05-31 10:44:19 +0800

    blog

--- src/components/Blog/blogData.tsx
+++ src/components/Blog/blogData.tsx
@@ -4,32 +4,67 @@ const blogData: Blog[] = [
   {
     id: 1,
     title: "Best UI components for modern websites",
+    paragraph:"",
     image: "/images/blog/blog-01.jpg",
+    author: {
+      name: "Samuyl Joshi",
+      image: "/images/blog/author-01.png",
+      designation: "Graphic Designer",
+    },
     tags: ["creative"],
+    publishDate: "",
   },
   {
     id: 2,
     title: "9 simple ways to improve your design skills",
+    paragraph:"",
     image: "/images/blog/blog-02.jpg",
+    author: {
+      name: "Musharof Chy",
+      image: "/images/blog/author-02.png",
+      designation: "Content Writer",
+    },
     tags: ["computer"],
+    publishDate: "",
   },
   {
     id: 3,
     title: "Tips to quickly improve your coding speed.",
+    paragraph:"",
     image: "/images/blog/blog-03.jpg",
+    author: {
+      name: "",
+      image: "",
+      designation: "",
+    },
     tags: ["design"],
+    publishDate: "",
   },
   {
     id: 4,
     title: "Tips to quickly improve your coding speed.",
+    paragraph:"",
     image: "/images/blog/blog-03.jpg",
-    tags: [""],
+    author: {
+      name: "",
+      image: "",
+      designation: "",
+    },
+    tags: ["design"],
+    publishDate: "",
   }, 
   {
     id: 5,
     title: "Tips to quickly improve your coding speed.",
+    paragraph:"",
     image: "/images/blog/blog-03.jpg",
+    author: {
+      name: "",
+      image: "",
+      designation: "",
+    },
     tags: ["design"],
+    publishDate: "",
   },
 ];
 export default blogData;
--- src/types/blog.ts
+++ src/types/blog.ts
@@ -7,17 +7,9 @@ type Author = {
 export type Blog = {
   id: number;
   title: string;
+  paragraph: string;
   image: string;
+  author: Author;
   tags: string[];
+  publishDate: string;
 };
-
-
-// export type Blog = {
-//   id: number;
-//   title: string;
-//   paragraph: string;
-//   image: string;
-//   author: Author;
-//   tags: string[];
-//   publishDate: string;
-// };
