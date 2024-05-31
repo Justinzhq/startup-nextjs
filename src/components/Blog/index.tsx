c38178ade441ed5d89d44b568a244f4f66798ab7
Justinzhq <justinzh.q@gmail.com>
2024-05-31 11:20:12 +0800

    Update index.tsx

--- src/components/Blog/index.tsx
+++ src/components/Blog/index.tsx
@@ -6,7 +6,7 @@ const Blog = () => {
   return (
     <section
       id="blog"
-      className="bg-gray-light dark:bg-bg-color-dark py-8 md:py-8 lg:py-8"
+      className="bg-gray-light dark:bg-bg-color-dark py-16 md:py-20 lg:py-28"
     >
       <div className="container">
         <SectionTitle
