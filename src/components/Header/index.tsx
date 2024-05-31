42f5cb6ecbd558b5b75b2de2249c09f8ec95db1c
Justinzhq <justinzh.q@gmail.com>
2024-06-01 02:19:58 +0800

    Update index.tsx

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -59,15 +59,15 @@ const Header = () => {
                 <Image
                   src="/images/logo/logo-2.svg"
                   alt="logo"
-                  width={120}
-                  height={16}
+                  width={140}
+                  height={30}
                   className="w-full dark:hidden"
                 />
                 <Image
                   src="/images/logo/logo.png"
                   alt="logo"
-                  width={120}
-                  height={16}
+                  width={140}
+                  height={30}
                   className="hidden w-full dark:block"
                 />
               </Link>
