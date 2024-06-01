01dc57c0bac70ef5dd34a0bcbc211564b8c68027
Justinzhq <justinzh.q@gmail.com>
2024-06-01 16:23:54 +0800

    Update index.tsx

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -214,7 +214,8 @@ const Footer = () => {
           <div className="h-px w-full bg-gradient-to-r from-transparent via-[#D2D8E183] to-transparent dark:via-[#959CB183]"></div>
           <div className="py-8">
             <p className="text-center text-base text-body-color dark:text-white">
-              Template by{" "}
+            © 2024 viSCADA.net All rights reserved.
+              {/* Template by{" "}
               <a
                 href="http://uideck.com"
                 target="_blank"
@@ -231,7 +232,7 @@ const Footer = () => {
                 className="hover:text-primary"
               >
                 Next.js Templates
-              </a>
+              </a> */}
             </p>
           </div>
         </div>
